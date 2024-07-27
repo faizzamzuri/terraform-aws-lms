f350e564b17d824a7bc243423902537f154f0776
Rahman <amanyus@gmail.com>
2024-07-27 10:19:58 +0800

    terraform fmt

--- database.tf
+++ database.tf
@@ -1,12 +1,12 @@
 resource "aws_db_instance" "database1" {
-  allocated_storage    = 20
-  db_name              = "database1"
-  engine               = "mariadb"
-  engine_version       = "10.11.8"
-  instance_class       = "db.t3.micro"
-  username             = "admin"
-  password             = "Database1!"
-  parameter_group_name = "default.mariadb10.11"
-  skip_final_snapshot  = true
+  allocated_storage     = 20
+  db_name               = "database1"
+  engine                = "mariadb"
+  engine_version        = "10.11.8"
+  instance_class        = "db.t3.micro"
+  username              = "admin"
+  password              = "Database1!"
+  parameter_group_name  = "default.mariadb10.11"
+  skip_final_snapshot   = true
   copy_tags_to_snapshot = true
 }
